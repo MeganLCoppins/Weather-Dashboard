d497f8f429fddff79dc1054bea78cf58c5bcd32c
Megan Coppins <meganlcoppins@gmail.com>
2020-02-16 21:34:07 -0700

    functions commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,72 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+$(document).ready(function(){
 
+var apiKey = "ec769fc248d61a50ac4a8124e2ff8391";
 var $temp = $("#temp");
 var $humidity = $("#humidity");
 var $wind = $("#wind");
+var $uv = $("#uv");
+var $city = $("#city");
 var $search = $("#search");
 var $searchBtn = $("#searchBtn");
+var $hide = $(".hide");
+
 
 // .on("click") function associated with the Search Button
-// Create an AJAX call
+
 $searchBtn.on("click", function(event){
     event.preventDefault();
 
-    var search = $search.val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + search + "&appid=ec769fc248d61a50ac4a8124e2ff8391";
+    $hide.display = "block";
 
+    var search = $search.val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + search + "&appid=" + apiKey;
+// Create an AJAX call to get current weather information
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        $temp.text(response.main.temp);
-        $humidity.text(response.main.humidity);
-        $wind.text(response.wind.speed);
-        console.log(response);
-    });
-})
+        $city.text(response.name);
+        $temp.text("Temperature: " + response.main.temp + " °F");
+        $humidity.text("Humidity: " + response.main.humidity + " %");
+        $wind.text("Wind Speed: " + response.wind.speed + " MPH");
+        weatherIconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+        weatherImage = $("<img>").attr("src", weatherIconUrl);
+        $city.append(weatherImage)
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
+        var uvIndexQueryUrl = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + apiKey + "&lat=" + latitude + "&lon=" + longitude;
+// create another AJAX call to get UV Index information
+        $.ajax({
+            url: uvIndexQueryUrl,
+            method: "GET"
+        }).then(function(returned){
+            $uv.text("UV Index: " + returned.value);
+        })
 
+// third AJAX call to get forecast information
+        var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + apiKey + "&units=imperial"
+
+        $.ajax({
+            url: forecastQueryURL,
+            method: "GET"
+        }).then(function(result){
+            console.log(result);
+
+    // temperature forecast - 5 days
+            $("#forecastTemp1").text("Temp: " + result.list[0].main.temp);
+            $("#forecastTemp2").text("Temp: " + result.list[1].main.temp);
+            $("#forecastTemp3").text("Temp: " + result.list[2].main.temp);
+            $("#forecastTemp4").text("Temp: " + result.list[3].main.temp);
+            $("#forecastTemp5").text("Temp: " + result.list[4].main.temp);
+    // humidity forecast - 5 days
+            $("#forecastHumidity1").text("Humidity: " + result.list[0].main.humidity);
+            $("#forecastHumidity2").text("Humidity: " + result.list[1].main.humidity);
+            $("#forecastHumidity3").text("Humidity: " + result.list[2].main.humidity);
+            $("#forecastHumidity4").text("Humidity: " + result.list[3].main.humidity);
+            $("#forecastHumidity5").text("Humidity: " + result.list[4].main.humidity);
+        })
+    });
+
+});
+
+});
