9c99ec91c5be931122b1e9443410c844914ec062
Megan Coppins <meganlcoppins@gmail.com>
2020-02-17 18:54:32 -0700

    for loop commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var apiKey = "ec769fc248d61a50ac4a8124e2ff8391";
+    var $temp = $("#temp");
+    var $humidity = $("#humidity");
+    var $wind = $("#wind");
+    var $uv = $("#uv");
+    var $city = $("#city");
+    var $search = $("#search");
+    var $searchBtn = $("#searchBtn");
+    
+    var searchArray = [];
+    getSearch();
 
-var apiKey = "ec769fc248d61a50ac4a8124e2ff8391";
-var $temp = $("#temp");
-var $humidity = $("#humidity");
-var $wind = $("#wind");
-var $uv = $("#uv");
-var $city = $("#city");
-var $search = $("#search");
-var $searchBtn = $("#searchBtn");
-var $hide = $("#hide");
-var $currentWeather = $("#currentWeather");
-
-
-// .on("click") function associated with the Search Button
+// on click event to grab search input $ display hidden div
     $searchBtn.on("click", function(event){
         event.preventDefault();
-    // display current weather and forecast when button is clicked
+
         $(".hide").removeClass("hide");
+        searchBtnCall();
+    });
 
-// Create an AJAX call to get current weather information
+
+// function built to create AJAX call from search form input
+    function searchBtnCall(){
+        var search = $search.val();
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + search + "&appid=" + apiKey;
-
+    // create AJAX call to get current weather information
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function(response) {
+        }).then(function(response){
             $city.text(response.name);
             $temp.text("Temperature: " + response.main.temp + " °F");
             $humidity.text("Humidity: " + response.main.humidity + " %");
             $wind.text("Wind Speed: " + response.wind.speed + " MPH");
             weatherIconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
             weatherImage = $("<img>").attr("src", weatherIconUrl);
-            $city.append(weatherImage)
+            $city.append(weatherImage);
             var latitude = response.coord.lat;
             var longitude = response.coord.lon;
             var uvIndexQueryUrl = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + apiKey + "&lat=" + latitude + "&lon=" +   longitude;
-// create another AJAX call to get UV Index information
+        // create another AJAX call to get UV Index information
             $.ajax({
                 url: uvIndexQueryUrl,
                 method: "GET"
@@ -51,60 +55,109 @@ var $currentWeather = $("#currentWeather");
                 if(returned.value > 7){
                     $uv.addClass("severe");
                 }
-            })
+            });
+        // third AJAX call to get forecast information
+            var forecastQueryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + apiKey + "&units=imperial"
 
-// third AJAX call to get forecast information
-        var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + apiKey + "&units=imperial"
-
-        $.ajax({
-            url: forecastQueryURL,
-            method: "GET"
-        }).then(function(result){
-            console.log(result);
-
-    // temperature forecast - 5 days
-            $("#forecastTemp1").text("Temp: " + result.list[0].main.temp);
-            $("#forecastTemp2").text("Temp: " + result.list[1].main.temp);
-            $("#forecastTemp3").text("Temp: " + result.list[2].main.temp);
-            $("#forecastTemp4").text("Temp: " + result.list[3].main.temp);
-            $("#forecastTemp5").text("Temp: " + result.list[4].main.temp);
-    // humidity forecast - 5 days
-            $("#forecastHumidity1").text("Humidity: " + result.list[0].main.humidity);
-            $("#forecastHumidity2").text("Humidity: " + result.list[1].main.humidity);
-            $("#forecastHumidity3").text("Humidity: " + result.list[2].main.humidity);
-            $("#forecastHumidity4").text("Humidity: " + result.list[3].main.humidity);
-            $("#forecastHumidity5").text("Humidity: " + result.list[4].main.humidity);
-        })
-    });
-// storing search value in local storage 
+            $.ajax({
+                url: forecastQueryUrl,
+                method: "GET"
+            }).then(function(result){
+                for(var i = 0; i < 6; i++){
+                    $("#forecastTemp" + i).text("Temp: " + result.list[i].main.temp);
+                    $("#forecastHumidity" + i).text("Humidity: " + result.list[i].main.humidity);
+                }
+            });
+        });
     storeSearch();
-});
+    }
 
+// function built to store search in localStorage
     function storeSearch(){
         var search = $search.val();
-        localStorage.setItem("search", JSON.stringify(search));
-
-    // return from function is submitted search is blank
-    if(search === ""){
-        return;
-    }
+        // localStorage.setItem("search", JSON.stringify(search));
+        searchArray.push($search.val());
+    // set array to local storage
+        localStorage.setItem("localSearch", JSON.stringify(searchArray));
+    // return from function early if submitted search is blank
+        if(search === ""){
+            return;
+        }
     // clear input from search form
-    $search.text= "";
+        $search.val("");
 
-    getSearch();
+        getSearch();
     };
 
-    function getSearch(){
-        var storedSearch = JSON.parse(localStorage.getItem("search"));
-    
-    // creating li and button elements
-        var div =$("<div>");
-        var button = $("<button>");
-        button.attr("class", "btn-lg btn-block").text(storedSearch);
 
-    // prepending li to DOM and appending button to li
+// function built to get search from localStorage
+    function getSearch() {
+        var storedSearch = JSON.parse(localStorage.getItem("localSearch"));
+    // clear out button area
+        $("#listOfCities").empty();
+    // run for loop to dynamically add div and button based on searchArray
+        for(var i = 0; i < storedSearch.length; i++){
+            var div = $("<div>");
+            var button = $("<button>");
+            button.attr("class", "btn-lg btn-block city").text(storedSearch[i]);
+            button.attr("id", storedSearch[i]);
+
+    // prepending div to DOM and appending button to div
         $("#listOfCities").prepend(div);
         div.append(button);
-    };
+        }
+
+// on click event for search history buttons
+    $(".city").on("click", function(event){
+        event.preventDefault();
+        var search = $(this).text();
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + search + "&appid=" + apiKey;
     
+    // create AJAX call to get current weather
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            $city.text(response.name);
+            $temp.text("Temperature: " + response.main.temp + " °F");
+            $humidity.text("Humidity: " + response.main.humidity + " %");
+            $wind.text("Wind Speed: " + response.wind.speed + " MPH");
+            weatherIconUrl = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"
+            weatherImage = $("<img>").attr("src", weatherIconUrl);
+            $city.append(weatherImage);
+            var latitude = response.coord.lat;
+            var longitude = response.coord.lon;
+            var uvIndexQueryUrl = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + apiKey + "&lat=" + latitude + "&lon=" +   longitude;
+        // create another AJAX call to get UV index information
+            $.ajax({
+                url: uvIndexQueryUrl,
+                method: "GET"
+            }).then(function(returned){
+                $uv.text("UV Index: " + returned.value);
+
+                if(returned.value <= 2){
+                    $uv.addClass("favorable");
+                };
+                if(returned.value > 2 && returned.value <= 7){
+                    $uv.addClass("moderate");
+                };
+                if(returned.value > 7){
+                    $uv.addClass("severe");
+                };
+            });
+        });
+    // third AJAX call to get forecast information
+        var forecastQueryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=" + apiKey + "&units=imperial"
+
+        $.ajax({
+            url: forecastQueryUrl,
+            method: "GET"
+        }).then(function(result){
+            for(var i = 0; i < 6; i++){
+                $("#forecastTemp" + i).text("Temp: " + result.list[i].main.temp);
+                $("#forecastHumidity" + i).text("Humidity: " + result.list[i].main.humidity);
+            };
+        });
+    });
+    };
 });
